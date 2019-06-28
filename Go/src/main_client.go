507afc786537e4fa1671ecf0a1e2c262d568b621
Daniel <danidiaza@gmail.com>
2019-06-28 14:46:44 -0600

    Agregar cliente con varias pruebas al mismo tiempo

--- Go/src/main_client.go
+++ Go/src/main_client.go
@@ -12,12 +12,13 @@ import (
 func main()  {
 
     var server, output_file string
-    var port, rep int
+    var port, rep, files int
 
     flag.StringVar(&server, "host", "localhost", "Dirección del servidor")
     flag.StringVar(&output_file, "output", "result.csv", "Nombre del archivo donde escribir el resultado")
     flag.IntVar(&port, "port", 9000, "Puerto del servidor")
     flag.IntVar(&rep, "rep", 100, "Cantidad de repticiones")
+    flag.IntVar(&files, "files", 1, "Cantidad de pruebas/archivos")
 
     flag.Parse()
 
@@ -27,22 +28,26 @@ func main()  {
     }
     defer file.Close()
 
-    log.Println("Iniciando prueba")
-    var size int
-    var time time.Duration
-
     file.WriteString("Size (B), Iter, Time (ns)\n")
 
-    status := 20
-    for i := 0; i < rep; i++ {
-        size, time = client.GetFile(server, port)
-        line := fmt.Sprintf("%d,%d,%d\n", size, i+1, time)
-        file.WriteString(line)
+    for n := 0; n < files; n++ {
 
-        if i % status == 0 {
-            fmt.Println("...repeticion", i)
+        log.Printf("Iniciando prueba %d\n", n)
+        var size int
+        var time time.Duration
+
+
+        status := 20
+        for i := 0; i < rep; i++ {
+            size, time = client.GetFile(server, port+n)
+            line := fmt.Sprintf("%d,%d,%d\n", size, i+1, time)
+            file.WriteString(line)
+
+            if i % status == 0 {
+                fmt.Println("...repeticion", i)
+            }
         }
+        log.Println("Fin, resultados escritos en", output_file)
     }
     file.WriteString("\n")
-    log.Println("Fin, resultados escritos en", output_file)
 }
