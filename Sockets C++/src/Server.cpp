ccfdb00c965d592965702d9268aa1895aca40a83
Daniel <danidiaza@gmail.com>
2019-06-30 18:55:31 -0600

    Cambiar el server para que maneje archivos binarios y use pthreads

--- Sockets C++/src/Server.cpp
+++ Sockets C++/src/Server.cpp
@@ -2,6 +2,14 @@
 #include "../lib/Socket.hpp"
 #include "../lib/FileManager.hpp"
 #include <sys/wait.h>
+#include <pthread.h>
+
+class pargs {
+public:
+    char *buffer;
+    int length;
+    Socket *s;
+};
 
 void debug(std::string data, int t = 0){
 	sleep(t);
@@ -27,6 +35,16 @@ std::string getDataMeasurement(int &size,std::string dm = "B"){
 	return dataMeasuremet[measure];
 }
 
+void* ProcessConnection(void* args) {
+    pargs* cast = (pargs*) args;
+    cast->s->Write(cast->buffer, cast->length);
+    cast->s->Close();
+
+    delete cast;
+    int tid = 0;
+    pthread_exit(NULL);
+}
+
 /**
  * arg1: port
  * arg2: file name
@@ -37,35 +55,45 @@ int main(int argc, char **argv){
 	int childpid;
 
 	// building socket
-	Socket s1('s',false), *s2;
+	Socket s1('s',false);
 	if(s1.Bind(std::atoi(argv[1]))==-1)exit(EXIT_FAILURE);
 	s1.Listen( 5 );
 
 	std::string filename = argv[2];
 	int connection = 0;
 
-	std::cout<<"\nServing the file \033[33m\""<< filename << "\"\033[0m.\n";	
-	forever{
-		std::cout<<"\033[sWaiting for client to connect on the port \033[33m"<<argv[1]<<"\033[0m.";	
-		childpid = fork();
-		if(childpid < 0)perror("server: fork error.");
-		else if (0 == childpid){ 
-			s2 = s1.Accept();
-			fileManager.open(filename);
-			std::string buffer = "";
-			int bufferSize = 0;
-			s1.Close();
-			do{
-				buffer = fileManager.readSome(BUF_SIZE);
-				bufferSize+=buffer.size();
-				s2->Write(buffer.c_str());	
-			}while(buffer.size() != 0);	
-			s2->Close();
-			fileManager.close();
-			_exit(0);
-		}
-		std::cout<<"\nConnections: \033[33m"<< ++connection <<"\033[0m.";
-		wait(0);
-		std::cout<<"\033[u\033[0K";
+	std::cout<<"\nServing the file \033[33m\""<< filename << "\"\033[0m.\n";
+
+    char *buffer;
+    int buffer_len;
+    fileManager.open(filename);
+    buffer = fileManager.readFile(&buffer_len);
+    fileManager.close();
+
+    std::cout<<"\033[sWaiting for client to connect on the port \033[33m"<<argv[1]<<"\033[0m." << std::endl;
+    int tid = 0;
+
+	forever {
+        pargs *args;
+        args = new pargs;
+
+        args->buffer = buffer;
+        args->length = buffer_len;
+        args->s = s1.Accept();
+
+        pthread_t hijo;
+        int rc = pthread_create(&hijo, NULL, ProcessConnection, (void*)args);
+        if (rc) {
+            std::cout << "Couldn't create thread" << std::endl;
+            exit(-1);
+        }
+
+        rc = pthread_detach(hijo);
+        if (rc) {
+            std::cout << "Couldn't detach thread" << std::endl;
+            exit(-1);
+        }
+
+        std::cout << "Spawned thread " << ++tid << std::endl;
 	}
 }
